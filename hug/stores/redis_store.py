72daeaeb396f02f72eeda703ecc01e34e5983708
abhi <maps.abhishek@gmail.com>
2025-02-12 19:22:32 +0000

    testing print

--- hug/stores/redis_store.py
+++ hug/stores/redis_store.py
@@ -1,10 +1,9 @@
 import redis
-from hug.logger_mixin import LoggerMixin
+import logging
 
 class RedisStore:
-    def __init__(self, host='localhost', port=6379, db=0, ttl=3600, logger_name="hug"):
-        super().__init__(logger_name)
-
+    def __init__(self, host='localhost', port=6379, db=0, ttl=3600, logger=None):
+        self._logger = logger if logger is not None else logging.getLogger("hug")
         self._client = redis.StrictRedis(host=host, port=port, db=db, decode_responses=True)
         self._ttl = ttl
 
@@ -12,10 +11,10 @@ class RedisStore:
         try:
             return self._client.hgetall(key)
         except redis.RedisError as e:
-            self._logger.exception(f"Redis Error: {e}")
+            print(f"Redis Error: {e}")
             return {}
         except Exception as e:
-            self._logger.exception(f"Redis Exception: {e}")
+            print(f"Redis Exception: {e}")
             return {}
 
     def set(self, key, data):
@@ -23,29 +22,29 @@ class RedisStore:
             self._client.hmset(key, data)
             self._client.expire(key, self._ttl)
         except redis.RedisError as e:
-            self._logger.exception(f"Redis Error: {e}")
+            print(f"Redis Error: {e}")
             raise
         except Exception as e:
-            self._logger.exception(f"Redis Exception: {e}")
+            print(f"Redis Exception: {e}")
             raise
 
     def exists(self, key):
         try:
             return self._client.exists(key)
         except redis.RedisError as e:
-            self._logger.exception(f"Redis Error: {e}")
+            print(f"Redis Error: {e}")
             raise
         except Exception as e:
-            self._logger.exception(f"Redis Exception: {e}")
+            print(f"Redis Exception: {e}")
             raise
 
     def delete(self, key):
         try:
             self._client.delete(key)
         except redis.RedisError as e:
-            self._logger.exception(f"Redis Error: {e}")
+            print(f"Redis Error: {e}")
             raise
         except Exception as e:
-            self._logger.exception(f"Redis Exception: {e}")
+            print(f"Redis Exception: {e}")
             raise
         
