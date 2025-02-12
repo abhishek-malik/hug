e1cf570b40c9486d9efdac6be36110f299bb5719
abhi <maps.abhishek@gmail.com>
2025-02-12 18:34:16 +0000

    exception handling fix

--- hug/stores/redis_store.py
+++ hug/stores/redis_store.py
@@ -11,7 +11,7 @@ class RedisStore:
         try:
             return self._client.hgetall(key)
         except redis.RedisError as e:
-            self._logger.error(f"Redis Error: {e}")
+            self._logger.exception(f"Redis Error: {e}")
             return {}
         except Exception as e:
             self._logger.exception(f"Redis Exception: {e}")
@@ -22,7 +22,7 @@ class RedisStore:
             self._client.hmset(key, data)
             self._client.expire(key, self._ttl)
         except redis.RedisError as e:
-            self._logger.error(f"Redis Error: {e}")
+            self._logger.exception(f"Redis Error: {e}")
             raise
         except Exception as e:
             self._logger.exception(f"Redis Exception: {e}")
@@ -32,7 +32,7 @@ class RedisStore:
         try:
             return self._client.exists(key)
         except redis.RedisError as e:
-            self._logger.error(f"Redis Error: {e}")
+            self._logger.exception(f"Redis Error: {e}")
             raise
         except Exception as e:
             self._logger.exception(f"Redis Exception: {e}")
